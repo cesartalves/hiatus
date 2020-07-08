0cc06709f3631b47bc65902423c1fb83cb584e3d
cesartalvez <cesartalvez@gmail.com>
2020-07-08 11:48:47 -0300

    gem scaffold

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in circuit_breaker.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
+gem "rspec", "~> 3.0"
---
+++ spec/circuit_breaker_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CircuitBreaker do
+  it "has a version number" do
+    expect(CircuitBreaker::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
